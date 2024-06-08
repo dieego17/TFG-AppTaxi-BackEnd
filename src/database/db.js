a038915e3d807eec5208dfda1848514d962d5963
dieego17 <diegorubio1704.drs@gmail.com>
2024-06-08 19:04:59 +0200

    change database name

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize('apptaxi','root','',{
+const sequelize = new Sequelize('apptaxio','root','',{
     host:"localhost",
     dialect:"mysql"
 })
